e778c5565b0464e332627dcbd32ca04c96b1e0ba
Gustavo Nunes <gustavonunesedl@gmail.com>
2015-09-08 15:22:24 -0300

    Enabling IDO; Activating SHIFT-based window switching; Showing line-numbers; Disabling startup screen and message.

--- config/general.el
+++ config/general.el
@@ -1,5 +1,18 @@
-; start maximized
+;; start maximized
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-; disable beep sounds
+;; disable beep sounds
 (setq visible-bell 1)
+
+;; show line numbers
+(global-linum-mode 1)
+
+;; activate IDO
+(ido-mode 1)
+
+;; hide splash screen and message
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
+
+;; activate SHIFT-based window switching
+(windmove-default-keybindings)
