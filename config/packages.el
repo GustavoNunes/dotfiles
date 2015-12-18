12aac4067af8a47b3f994fbf9f73b82b8123a90a
Gustavo Nunes <gustavonunesedl@gmail.com>
2015-12-18 15:20:46 -0200

    Added haskell-mode and melpa-stable repository.

--- config/packages.el
+++ config/packages.el
@@ -1,11 +1,13 @@
 ; list the packages you want
-(setq package-list '(php-mode web-mode markdown-mode auto-complete let-alist geben fill-column-indicator magit zenburn-theme doremi doremi-cmd php-eldoc))
+(setq package-list '(php-mode web-mode markdown-mode auto-complete let-alist geben fill-column-indicator magit zenburn-theme doremi doremi-cmd php-eldoc haskell-mode))
 
 ; list the repositories containing them
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
+
 
 ; activate all the packages (in particular autoloads)
 (package-initialize)
