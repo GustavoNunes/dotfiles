9d2ac26073d32fdbb012a108c31992cca76e9777
Gustavo Nunes <gustavonunesedl@gmail.com>
2015-12-21 16:25:08 -0200

    Added "restclient" package.

--- config/packages.el
+++ config/packages.el
@@ -1,5 +1,5 @@
 ; list the packages you want
-(setq package-list '(php-mode web-mode markdown-mode auto-complete let-alist geben fill-column-indicator magit zenburn-theme doremi doremi-cmd php-eldoc haskell-mode hide-comnt))
+(setq package-list '(php-mode web-mode markdown-mode auto-complete let-alist geben fill-column-indicator magit zenburn-theme doremi doremi-cmd php-eldoc haskell-mode hide-comnt restclient))
 
 ; list the repositories containing them
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
