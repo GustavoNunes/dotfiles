0df4b96faf91d0fa3cb6102007fff5fc5e359081
Gustavo Nunes <gustavonunesedl@gmail.com>
2016-06-01 19:46:22 -0300

    Customized some faces

--- config/display.el
+++ config/display.el
@@ -8,3 +8,12 @@
 
 ;; line numbers spacing
 (setq linum-format "%4d  ")
+
+(custom-set-faces
+ '(avy-background-face ((t (:background "#3F3F3F" :foreground "gray60" :inverse-video nil))))
+ '(avy-lead-face ((t (:foreground "green"))))
+ '(avy-lead-face-0 ((t (:foreground "green"))))
+ '(avy-lead-face-1 ((t (:foreground "green"))))
+ '(avy-lead-face-2 ((t (:foreground "green"))))
+ '(mode-line ((t (:background "#2B2B2B" :foreground "#8FB28F" :box (:line-width 1 :style released-button)))))
+ '(mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width 1 :style released-button))))))
