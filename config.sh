1aa15a3e1fab9056335eb7567ca2dbd87424cb83
Gustavo Nunes <gustavonunesed@gmail.com>
2016-10-05 11:34:42 -0300

    Refactored dotfiles folder

--- config.sh
+++ config.sh
@@ -1,21 +1,20 @@
 # Emacs configuration
 git clone https://github.com/syl20bnr/spacemacs /home/gustavo/.emacs.d
 chown -R gustavo:users /home/gustavo/.emacs.d
-ln -sfn $PWD/.spacemacs /home/gustavo/.spacemacs
+rm /home/gustavo/.spacemacs
+ln -sfn $PWD/.spacemacs.d /home/gustavo/.spacemacs.d
 
 # NixOS configuration
-ln -sfn $PWD/etc/nixos /etc/nixos
+ln -sfn $PWD/nixos /etc/nixos
 
 # i3 configuration
-ln -sfn $PWD/.config/i3/config /home/gustavo/.config/i3/config
-ln -sfn $PWD/.i3status.conf /home/gustavo/.i3status.conf
+ln -sfn $PWD/i3/config /home/gustavo/.config/i3/config
+ln -sfn $PWD/i3/.i3status.conf /home/gustavo/.i3status.conf
+ln -sfn $PWD/i3/wallpaper.jpg /home/gustavo/wallpaper.jpg
 
 # X11 configuration
-ln -sfn $PWD/.Xresources /home/gustavo/.Xresources
-
-# Misc configuration
-ln -sfn $PWD/wallpaper.jpg /home/gustavo/wallpaper.jpg
+ln -sfn $PWD/X11/.Xresources /home/gustavo/.Xresources
 
 # urxvt configuration
 mkdir -p /home/gustavo/.urxvt/ext
-ln -sfn $PWD/urxvt-clipboard /home/gustavo/.urxvt/ext/clipboard
+ln -sfn $PWD/urxvt/urxvt-clipboard /home/gustavo/.urxvt/ext/clipboard
